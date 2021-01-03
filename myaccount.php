7c190d762cc01a8100fa7b576f375ee21f930024
shimwabobs <shimwabonheur00@gmail.com>
2021-01-03 09:56:30 +0200

    rechanged

---
+++ myaccount.php
@@ -0,0 +1,24 @@
+<?php
+include("auth_session.php");
+?>
+<?php
+include("db.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bob store | My account</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+</head>
+<body>
+<div class="form">
+    <p>Name: <?php echo $_SESSION['username']; ?></p>
+</div>        
+</body>
+</html>
